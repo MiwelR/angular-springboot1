ab1de336cbecf3a01a567a26ef20f6502686f077
MiwelR <rrriosss@gmail.com>
2021-08-02 11:40:37 +0200

    # Modificación para desplegar

--- src/app/_services/hello.service.ts
+++ src/app/_services/hello.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'https://alansastre-springboot-jwt.herokuapp.com/api/';
+const API_URL = 'https://miwelr-springboot-postgresql.herokuapp.com/api/';
+//const API_URL = 'http://localhost:8080/api/auth/';
 
 @Injectable({
   providedIn: 'root'
